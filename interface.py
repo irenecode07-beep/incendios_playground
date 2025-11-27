e3507ea802acb4763b43d248a6c12699615cd889
irenecode07-beep <irenecode07@gmail.com>
2025-11-27 22:19:32 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -8,7 +8,6 @@ Original file is located at
 """
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 from scipy import stats
